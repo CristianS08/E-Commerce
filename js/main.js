12f429eda1e8f5d661d28ba3ad4f052c27715d99
Cristian Suarez <cristians0898@hotmail.com>
2022-08-09 23:35:34 -0300

    Entrega sobre eventos y DOM

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+/* 
 class Product{
     constructor(type, price, description, image){
         this.price = parseFloat(price);
@@ -61,4 +61,78 @@ function findByPrice (downPrice, topPrice){
     }
 
     return foundProducts;
+} */
+
+
+
+
+
+//Log in 
+
+function logInErrorMesage(){
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let error = "";
+
+    if(email.value == ""){
+        error = "<p class = 'text-white bg-danger'> El campo <strong>Email address</strong> no puede ir vacio </p>"
+        document.getElementById("emailError").innerHTML = error;
+    }
+    
+    error = "";
+
+    if(password.value == ""){
+        error = "<p class = 'text-white bg-danger'> El campo <strong>Password</strong> no puede ir vacio </p>"
+        document.getElementById("passwordError").innerHTML = error;
+    }
+
+    error = "";
+    
 }
+
+let button = document.getElementById("button")
+button.addEventListener("click", logInErrorMesage);
+
+
+
+
+//registration
+
+/* function registrationErrorMesage(){
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let name = document.getElementById("name");
+    let last_name = document.getElementById("lastName");
+    let error = "";
+
+    if(email.value == ""){
+        error = "<p class = 'text-white bg-danger'> El campo <strong>Email address</strong> no puede ir vacio </p>"
+        document.getElementById("emailError").innerHTML = error;
+    }
+    
+    error = "";
+
+    if(password.value == ""){
+        error = "<p class = 'text-white bg-danger'> El campo <strong>Password</strong> no puede ir vacio </p>"
+        document.getElementById("passwordError").innerHTML = error;
+    }
+
+    error = "";
+
+    if(name.value == ""){
+        error = "<p class = 'text-white bg-danger'> El campo <strong>Name</strong> no puede ir vacio </p>"
+        document.getElementById("nameError").innerHTML = error;
+    }
+
+    error = "";
+
+    if(last_name.value == ""){
+        error = "<p class = 'text-white bg-danger'> El campo <strong>Last name</strong> no puede ir vacio </p>"
+        document.getElementById("lastNameError").innerHTML = error;
+    }
+
+    error = "";
+}
+
+let signInButton = document.getElementById("signInButton");
+signInButton.addEventListener("click", registrationErrorMesage); */
